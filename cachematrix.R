bcc6a0a3ce6d3058e875664a792646fd0fe90fd9
gxzhang1992 <zhanggo5@msu.edu>
2016-10-07 15:45:40 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,12 @@
 
+## This function is used to makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+##set the value of the vector
+##get the value of the vector
+##set the value of the mean
+##get the value of the mean ##
+
 makeCacheMatrix <- function(x = matrix()) {
+  ## initialize the value of the matrix inverse to NULL 
   matrixinverse <- NULL
   set <- function(y) {
     x <<- y
@@ -14,13 +21,15 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-
+# used to get the cache of the matrix
 cacheSolve <- function(x, ...) {
   matrixinverse <- x$getmatrixinverse()
+  ## if the data exists in cache, it shows "get cache data - unversematrix"
   if(!is.null(matrixinverse)){
     message("get cached data - InverseMatrix")
     return(matrixinverse)
   }
+  ## if not, it will calculate and store the data to cache
   data <- x$get()
   matrixinverse <- solve(data, ...)
   x$setmatrixinverse(matrixinverse)
